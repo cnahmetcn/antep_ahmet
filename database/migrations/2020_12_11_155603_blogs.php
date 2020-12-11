d4e1bf0daceabf228669b0523bf3c33103567412
Ahmet CAN <cnahmet@outlook.com>
2020-12-11 19:21:25 +0300

    son degisiklikler

--- database/migrations/2020_12_11_155603_blogs.php
+++ database/migrations/2020_12_11_155603_blogs.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBlogsTable extends Migration
+class Blogs extends Migration
 {
     /**
      * Run the migrations.
@@ -16,7 +16,10 @@ class CreateBlogsTable extends Migration
         Schema::create('blogs', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
-            $table->string('description');
+            $table->string('photo');
+            $table->text('content');
+            $table->dateTime('datetime');
+            $table->integer('comment')->default(0);
             $table->timestamps();
         });
     }
--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -9,21 +9,21 @@
         </div>
         <div class="row">
             @foreach($blogs as $b)
-
-                <div class="col-lg-4 col-md-4 col-sm-6">
-                    <div class="blog__item">
-                        <div class="blog__item__pic">
-                            <img src="img/blog/blog-1.jpg" alt="">
-                        </div>
-                        <div class="blog__item__text">
-                            <ul>
-                                <li><i class="fa fa-calendar-o"></i>{{$b->created_at}}</li>
-                            </ul>
-                            <h5><a href="#">{{$b->title}}</a></h5>
-                            <p>{{$b->description}} </p>
-                        </div>
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="blog__item">
+                    <div class="blog__item__pic">
+                        <img src="img/blog/{{$b->photo}}" alt="">
+                    </div>
+                    <div class="blog__item__text">
+                        <ul>
+                            <li><i class="fa fa-calendar-o"></i> {{date('d-m-Y', strtotime($b->datetime))}}</li>
+                            <li><i class="fa fa-comment-o"></i> {{$b->comment}}</li>
+                        </ul>
+                        <h5><a href="#">{{$b->title}}</a></h5>
+                        <p>{{$b->content}} </p>
                     </div>
                 </div>
+            </div>
             @endforeach
         </div>
     </div>
